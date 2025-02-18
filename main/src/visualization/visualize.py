d0ace7cbeefb28ec8479619487cf40dcf211cf8e
Daryn Bằng <141810099+DarynBang@users.noreply.github.com>
2025-02-18 14:53:03 +0700

    Update visualize.py

--- main/src/visualization/visualize.py
+++ main/src/visualization/visualize.py
@@ -1,4 +1,5 @@
 import glob
+from utils import 
 
 path_3ds = glob("MNIST/*3d*")
 
@@ -22,8 +23,6 @@ for idx, path in enumerate(path_3ds):
   test_labels.append(np.ones(len(data_test_images))*idx)
   labels_map[path.split("/")[-1].split(".")[0]] = idx
 
-
-
 train_data = np.concatenate(train_data)
 train_labels = np.concatenate(train_labels)
 val_data = np.concatenate(val_data)
@@ -34,3 +33,52 @@ test_labels = np.concatenate(test_labels)
 train_data, train_labels = shuffle(train_data, train_labels, random_state=42)
 val_data, val_labels = shuffle(val_data, val_labels, random_state=42)
 test_data, test_labels = shuffle(test_data, test_labels, random_state=42)
+
+# Plot a few images 
+plot_images_with_labels(train_data, train_labels, labels_map, num_images=16)
+
+# Plot images from synapsemnist3d
+data = np.load('MNIST/synapsemnist3d.npz')
+train_images = data["train_images"]
+images = train_images
+image  = images[0]
+print("Dimension of the CT scan is:", image.shape)
+plt.imshow(np.squeeze(image[:, :, 20]), cmap="gray")
+plot_slices(4, 7, 28, 28, image[:, :, :40])
+
+# Plot images from nodulemnist3d
+data = np.load('MNIST/nodulemnist3d.npz')
+train_images = data["train_images"]
+images = train_images
+image  = images[0]
+print("Dimension of the CT scan is:", image.shape)
+plt.imshow(np.squeeze(image[:, :, 20]), cmap="gray")
+plot_slices(4, 7, 28, 28, image[:, :, :40])
+
+# Plot images from adrenalmnist3d
+data = np.load('MNIST/adrenalmnist3d.npz')
+train_images = data["train_images"]
+images = train_images
+image  = images[0]
+print("Dimension of the CT scan is:", image.shape)
+plt.imshow(np.squeeze(image[:, :, 20]), cmap="gray")
+plot_slices(4, 7, 28, 28, image[:, :, :40])
+
+# Plot images from fracturemnist3d
+data = np.load('MNIST/fracturemnist3d.npz')
+train_images = data["train_images"]
+images = train_images
+image  = images[0]
+print("Dimension of the CT scan is:", image.shape)
+plt.imshow(np.squeeze(image[:, :, 20]), cmap="gray")
+plot_slices(4, 7, 28, 28, image[:, :, :40])
+
+# Plot images from organmnist3d
+data = np.load('MNIST/organmnist3d.npz')
+train_images = data["train_images"]
+images = train_images
+image  = images[0]
+print("Dimension of the CT scan is:", image.shape)
+plt.imshow(np.squeeze(image[:, :, 20]), cmap="gray")
+plot_slices(4, 7, 28, 28, image[:, :, :40])
+
