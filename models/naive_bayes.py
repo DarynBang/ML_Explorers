db472a068fa49492b51e3f8cc16cdccb365eec0d
Daryn Bằng <141810099+DarynBang@users.noreply.github.com>
2025-02-20 18:10:45 +0700

    Update naive_bayes.py

--- models/naive_bayes.py
+++ models/naive_bayes.py
@@ -29,3 +29,8 @@ def naive_bayes_algorithm(embedding_name= 'flatten'):
   print(f"Precision: {precision:.4f}")
   print(f"Recall: {recall:.4f}")
   print(f"F1 Score: {f1:.4f}")
+
+if __name__ == '__main__:
+  models_name = ['flatten', '2D_CNN_init', '2D_CNN_pretrained', '3D_CNN_init', '3D_CNN_pretrained']
+  for model in models_name:
+    naive_bayes_algorithm(embedding_name = model)
