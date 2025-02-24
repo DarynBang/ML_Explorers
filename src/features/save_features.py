1c2188fbd139692d2d82bee6a1fcf5cbd2701c41
Daryn Bằng <141810099+DarynBang@users.noreply.github.com>
2025-02-24 10:13:05 +0700

    Update save_features.py

--- src/features/save_features.py
+++ src/features/save_features.py
@@ -25,6 +25,29 @@ torch.save(val_labels,          "Embedding/flatten_val_labels.pt")
 torch.save(test_labels,         "Embedding/flatten_test_labels.pt")
 
 
+def get_embeddings(model, loader):
+    vectors = []
+        
+    with torch.no_grad():
+        for inputs, labels in loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            vectors.append(model.get_embedding(inputs))
+        vectors = torch.concatenate(vectors).detach().cpu().numpy()
+
+    return vectors
+    
+
+def get_embeddings_from_loaders(train_loader, val_loader, test_loader, model, model_path = None, pretrained=False):
+    if pretrained and model_path is not None:
+        model = model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+    train_vectors, val_vectors, test_vectors = get_embeddings(model, train_loader), get_embeddings(model, val_loader), get_embeddings(model, test_loader)
+    
+
+    return train_vectors, val_vectors, test_vectors
+    
+
 train2D_dataset = Custom2DDataset(train_data, train_labels)
 val2D_dataset = Custom2DDataset(val_data, val_labels)
 test2D_dataset = Custom2DDataset(test_data, test_labels)
@@ -34,37 +57,16 @@ val2D_loader = DataLoader(val2D_dataset, batch_size=512, shuffle=False)
 test2D_loader = DataLoader(test2D_dataset, batch_size=512, shuffle=False)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load models
+# Initialize model
 model_2D = CNN2D_MLP(num_classes=6).to(device)
 model_2D.eval()
 
-vectors2D_train = []
-vectors2D_val = []
-vectors2D_test = []
-
-# Get train embeddings from 2D model
-with torch.no_grad():
-    for inputs, labels in train2D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors2D_train.append(model_2D.get_embedding(inputs))
-    vectors2D_train = torch.concatenate(vectors2D_train).detach().cpu().numpy()
-    print(f'Train Embeddings after 2D Model: {vectors2D_train.shape}')
-
-# Get validation embeddings from 2D model
-with torch.no_grad():
-    for inputs, labels in val2D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors2D_val.append(model_2D.get_embedding(inputs))
-    vectors2D_val = torch.concatenate(vectors2D_val).detach().cpu().numpy()
-    print(f'Val Embeddings after 2D Model: {vectors2D_val.shape}')
-
-# Get test embeddings from 2D model
-with torch.no_grad():
-    for inputs, labels in test2D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors2D_test.append(model_2D.get_embedding(inputs))
-    vectors2D_test = torch.concatenate(vectors2D_test).detach().cpu().numpy()
-    print(f'Test Embeddings after 2D Model: {vectors2D_test.shape}')
+# Save embeddings from the initial 2D data
+vectors2D_train, vectors2D_val, vectors2D_test  = get_embeddings_from_loaders(train2D_loader, val2D_loader, test2D_loader, model_2D, None, False)
+# Print the embedding shapes
+print(f'Train Embeddings after 2D Model: {vectors2D_train.shape}')
+print(f'Val Embeddings after 2D Model: {vectors2D_val.shape}')
+print(f'Test Embeddings after 2D Model: {vectors2D_test.shape}')
 
 # Save initial 2D data
 torch.save(vectors2D_train,     "Embedding/2D_CNN_init_train_data.pt")
@@ -75,39 +77,18 @@ torch.save(val_labels,          "Embedding/2D_CNN_init_val_labels.pt")
 torch.save(test_labels,         "Embedding/2D_CNN_init_test_labels.pt")
 
 
-# Load states of trained models
-model_2D = CNN2D_MLP(num_classes=6).to(device)
-model_2D.load_state_dict(torch.load("best_model_2D_CNN.pth"))
-model_2D.eval()
+# Save embeddings from the train 2D model
+vectors2D_train, vectors2D_val, vectors2D_test  = get_embeddings_from_loaders(train2D_loader,
+                                                                              val2D_loader,
+                                                                              test2D_loader,
+                                                                              model_2D,
+                                                                              "best_model_2D_CNN.pth",
+                                                                              True)
+# Print the embedding shapes
+print(f'Train Embeddings after 2D Model: {vectors2D_train.shape}')
+print(f'Val Embeddings after 2D Model: {vectors2D_val.shape}')
+print(f'Test Embeddings after 2D Model: {vectors2D_test.shape}')
 
-
-vectors2D_train = []
-vectors2D_val = []
-vectors2D_test = []
-
-# Get train embeddings from 2D model
-with torch.no_grad():
-    for inputs, labels in train2D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors2D_train.append(model_2D.get_embedding(inputs))
-    vectors2D_train = torch.concatenate(vectors2D_train).detach().cpu().numpy()
-    print(f'Train Embeddings after 2D Model: {vectors2D_train.shape}')
-
-# Get validation embeddings from 2D model
-with torch.no_grad():
-    for inputs, labels in val2D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors2D_val.append(model_2D.get_embedding(inputs))
-    vectors2D_val = torch.concatenate(vectors2D_val).detach().cpu().numpy()
-    print(f'Val Embeddings after 2D Model: {vectors2D_val.shape}')
-
-# Get test embeddings from 2D model
-with torch.no_grad():
-    for inputs, labels in test2D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors2D_test.append(model_2D.get_embedding(inputs))
-    vectors2D_test = torch.concatenate(vectors2D_test).detach().cpu().numpy()
-    print(f'Test Embeddings after 2D Model: {vectors2D_test.shape}')
 
 # Save trained embeddings from 2D model
 torch.save(vectors2D_train,     "Embedding/2D_CNN_pretrained_train_data.pt")
@@ -126,42 +107,18 @@ train3D_loader = DataLoader(train3D_dataset, batch_size=512, shuffle=False)
 val3D_loader = DataLoader(val3D_dataset, batch_size=512, shuffle=False)
 test3D_loader = DataLoader(test3D_dataset, batch_size=512, shuffle=False)
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 # Load models
 model_3D = CNN3D_MLP(num_classes=6).to(device)
 model_3D.eval()
 
-vectors3D_train = []
-vectors3D_val = []
-vectors3D_test = []
+# Save initial 3D embeddings
+vectors3D_train, vectors3D_val, vectors3D_test  = get_embeddings_from_loaders(train3D_loader,
+                                                                              val3D_loader,
+                                                                              test3D_loader,
+                                                                              model_3D,
+                                                                              None,
+                                                                              False)
 
-# Get Train Embeddings from 3D model
-with torch.no_grad():
-    for inputs, labels in train3D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors3D_train.append(model_3D.embedding(inputs))
-    vectors3D_train = torch.concatenate(vectors3D_train).detach().cpu().numpy()
-    print(f'Train Embeddings after 3D Model: {vectors3D_train.shape}')
-
-# Get Validation Embeddings from 3D model
-with torch.no_grad():
-    for inputs, labels in val3D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors3D_val.append(model_3D.embedding(inputs))
-    vectors3D_val = torch.concatenate(vectors3D_val).detach().cpu().numpy()
-    print(f'Val Embeddings after 3D Model: {vectors3D_val.shape}')
-
-# Get Test Embeddings from 2D model
-with torch.no_grad():
-    for inputs, labels in test3D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors3D_test.append(model_3D.embedding(inputs))
-    vectors3D_test = torch.concatenate(vectors3D_test).detach().cpu().numpy()
-    print(f'Test Embeddings after 3D Model: {vectors3D_test.shape}')
-
-# Save initial 3D data
 torch.save(vectors3D_train,     "Embedding/3D_CNN_init_train_data.pt")
 torch.save(vectors3D_val,       "Embedding/3D_CNN_init_val_data.pt")
 torch.save(vectors3D_test,      "Embedding/3D_CNN_init_test_data.pt")
@@ -170,39 +127,14 @@ torch.save(val_labels,          "Embedding/3D_CNN_init_val_labels.pt")
 torch.save(test_labels,         "Embedding/3D_CNN_init_test_labels.pt")
 
 
-# Load states of trained models
 model_3D = CNN3D_MLP(num_classes=6).to(device)
-model_3D.load_state_dict(torch.load("best_model_3D_CNN.pth"))
-model_3D.eval()
 
-vectors3D_train = []
-vectors3D_val = []
-vectors3D_test = []
-
-# Get Train Embeddings from 3D model
-with torch.no_grad():
-    for inputs, labels in train3D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors3D_train.append(model_3D.embedding(inputs))
-    vectors3D_train = torch.concatenate(vectors3D_train).detach().cpu().numpy()
-    print(f'Train Embeddings after 3D Model: {vectors3D_train.shape}')
-
-# Get Validation Embeddings from 3D model
-with torch.no_grad():
-    for inputs, labels in val3D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors3D_val.append(model_3D.embedding(inputs))
-    vectors3D_val = torch.concatenate(vectors3D_val).detach().cpu().numpy()
-    print(f'Val Embeddings after 3D Model: {vectors3D_val.shape}')
-
-# Get Test Embeddings from 3D model
-with torch.no_grad():
-    for inputs, labels in test3D_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        vectors3D_test.append(model_3D.embedding(inputs))
-    vectors3D_test = torch.concatenate(vectors3D_test).detach().cpu().numpy()
-    print(f'Test Embeddings after 3D Model: {vectors3D_test.shape}')
-
+vectors3D_train, vectors3D_val, vectors3D_test  = get_embeddings_from_loaders(train3D_loader,
+                                                                              val3D_loader,
+                                                                              test3D_loader,
+                                                                              model_3D,
+                                                                              "best_model_3D_CNN.pth",
+                                                                              True)
 
 # Save trained embeddings from 3D model
 torch.save(vectors3D_train,     "Embedding/3D_CNN_pretrained_train_data.pt")
