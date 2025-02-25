d632600a3836a0e09d841ea51c326e12c937854e
Daryn Bằng <141810099+DarynBang@users.noreply.github.com>
2025-02-25 20:11:04 +0700

    Update MLP.py

--- src/models/MLP.py
+++ src/models/MLP.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score,
                              roc_auc_score, average_precision_score, roc_curve, precision_recall_curve)
-from utils import get_data_from_file
+from src.utils import get_data_from_file
 from src.data.make_dataset import CustomMLPDataset
 
 class Adaptive_MLP(nn.Module):
