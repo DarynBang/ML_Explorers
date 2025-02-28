a8757281ddc83e4e0139c25230ac0126e07d3850
Daryn Bằng <141810099+DarynBang@users.noreply.github.com>
2025-02-28 19:31:32 +0700

    Update HMM.py

--- src/models/HMM.py
+++ src/models/HMM.py
@@ -3,7 +3,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,
@@ -11,7 +10,6 @@ from src.utils import get_data_from_file
 import pandas as pd
 import pickle
 from hmmlearn import hmm
-import operator
 
 
 def hidden_markov_model_algorithm(embedding_name= 'flatten'):
